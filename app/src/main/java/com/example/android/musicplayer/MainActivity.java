7e649e31c9f79d6b94c1636151834f0a6f55471d
anne6 <anne.chatin@gmail.com>
2019-03-07 11:18:51 -0800

    add all music player features

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.musicplayer;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +12,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     public static final String LOG_TAG = MainActivity.class.getSimpleName();
+    private MediaPlayer mediaPlayer;
+    private AudioManager audio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         /**
          * How to implement an android MediaPlayer ?
          */
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.sorry_seems_to_be_the_hardest_word);
+        audio = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
 
         /**
          * Add button Listener on PLAY button
@@ -27,17 +34,45 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.d(LOG_TAG, "In OnClick() Play button");
+                mediaPlayer.start();
             }
         });
 
         /**
-         * Add button Listener on PLAY button
+         * Add button Listener on PAUSE button
          */
         Button pauseButton = findViewById(R.id.pause_button);
         pauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d(LOG_TAG, "In OnClick() Pause button");
+                mediaPlayer.pause();
+                }
+        });
+
+        /**
+         * Add button Listener on + button
+         */
+        Button plusButton = findViewById(R.id.plus_button);
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "In OnClick() + button");
+                audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                        AudioManager.ADJUST_RAISE, AudioManager.FLAG_SHOW_UI);
+            }
+        });
+
+        /**
+         * Add button Listener on - button
+         */
+        Button minusButton = findViewById(R.id.minus_button);
+        minusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "In OnClick() - button");
+                audio.adjustStreamVolume(AudioManager.STREAM_MUSIC,
+                        AudioManager.ADJUST_LOWER, AudioManager.FLAG_SHOW_UI);
             }
         });
     }
