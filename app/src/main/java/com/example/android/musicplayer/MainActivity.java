4dd6b0741caa5309d2431f971f3f9676622fbf2f
anne6 <anne.chatin@gmail.com>
2019-03-07 10:46:18 -0800

    Buttons and Listeners added

--- app/src/main/java/com/example/android/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/android/musicplayer/MainActivity.java
@@ -2,8 +2,13 @@ package com.example.android.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String LOG_TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +18,27 @@ public class MainActivity extends AppCompatActivity {
         /**
          * How to implement an android MediaPlayer ?
          */
+
+        /**
+         * Add button Listener on PLAY button
+         */
+        Button playButton = findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "In OnClick() Play button");
+            }
+        });
+
+        /**
+         * Add button Listener on PLAY button
+         */
+        Button pauseButton = findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(LOG_TAG, "In OnClick() Pause button");
+            }
+        });
     }
 }
